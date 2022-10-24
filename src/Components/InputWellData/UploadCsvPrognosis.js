3504f9d711df0dfbcd9e88e0f7caf1bee3b169dd
aralfayy <aralfayy@students.itb.ac.id>
2022-10-24 09:59:02 +0700

    heheh

--- src/Components/InputWellData/UploadCsvPrognosis.js
+++ src/Components/InputWellData/UploadCsvPrognosis.js
@@ -16,6 +16,7 @@ import { styled } from "@mui/material/styles";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import { Delete } from "@mui/icons-material";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -56,42 +57,73 @@ const UploadCsvPrognosis = () => {
   const [offsetWellCalibration, setOffsetWellCalibration] = useState("");
   const navigate = useNavigate();
 
+  const [deleteData, setDeleteData] = useState([]);
+
   const { id } = useParams();
 
   const saveDepth = async (e) => {
     e.preventDefault();
 
     const dataMap = csvData.map((res, i) => ({
-      wellName,
+      wellName: res.wellName,
       depth: parseInt(res.depth),
       twt: parseInt(res.twt),
-      area,
-      perimeter,
+      area: res.area,
+      perimeter: res.perimeter,
       resolution: parseInt(res.resolution),
-      reservoir,
-      survey,
-      wellRes: parseInt(wellRes),
-      horizonName,
+      reservoir: res.reservoir,
+      survey: res.survey,
+      wellRes: parseInt(res.wellRes),
+      horizonName: res.horizonName,
       avgAmp: parseInt(res.avgAmp),
       minAmp: parseInt(res.minAmp),
       medAmp: parseInt(res.medAmp),
       maxAmp: parseInt(res.maxAmp),
       offsetWellCalibration: parseInt(res.offsetWellCalibration),
     }));
-    console.log(dataMap);
 
     dataMap.map(async (res, i) => {
+      // console.log(res);
       try {
         await axios.post(`http://localhost:5000/prognosis/`, res);
+        navigate(`/WellDataPrognosis`);
       } catch (error) {
         console.log(error);
       }
     });
 
     // listDepth.push(dataMap);
-
-    navigate(`/WellDataPrognosis`);
   };
+  const deleteDepth = (index) => {
+    const delData = csvData.splice(index, 1);
+    setDeleteData(delData);
+    deleteData();
+    console.log(csvData);
+  };
+  // const deleteDepth = async () => {
+  //   const dataMap = csvData.map((res, i) => ({
+  //     wellName: res.wellName,
+  //     depth: parseInt(res.depth),
+  //     twt: parseInt(res.twt),
+  //     area: res.area,
+  //     perimeter: res.perimeter,
+  //     resolution: parseInt(res.resolution),
+  //     reservoir: res.reservoir,
+  //     survey: res.survey,
+  //     wellRes: parseInt(res.wellRes),
+  //     horizonName: res.horizonName,
+  //     avgAmp: parseInt(res.avgAmp),
+  //     minAmp: parseInt(res.minAmp),
+  //     medAmp: parseInt(res.medAmp),
+  //     maxAmp: parseInt(res.maxAmp),
+  //     offsetWellCalibration: parseInt(res.offsetWellCalibration),
+  //   }));
+
+  //   dataMap.map(async (index) => {
+  //     // console.log(res);
+  //     csvData.splice(index, 1);
+  //   });
+  // };
 
   return (
     <div>
@@ -108,8 +140,9 @@ const UploadCsvPrognosis = () => {
           Array.from(e.dataTransfer.files).map(async (file) => {
             let text = await file.text();
             let res = parse(text, { header: true });
+            res.data.pop();
             setCsvData(res.data);
-            console.log(res.data);
+            // setCsvData(csvData.pop());
           });
         }}
         onSubmit={saveDepth}
@@ -144,10 +177,11 @@ const UploadCsvPrognosis = () => {
               <StyledTableCell align="right">
                 Offset Well Calibration
               </StyledTableCell>
+              <StyledTableCell align="right">Action</StyledTableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {csvData.map((well) => (
+            {csvData.map((well, i) => (
               <StyledTableRow key={well._id}>
                 <StyledTableCell component="th" scope="row">
                   {well.wellName}
@@ -194,6 +228,13 @@ const UploadCsvPrognosis = () => {
                 <StyledTableCell component="th" scope="row">
                   {well.offsetWellCalibration}
                 </StyledTableCell>
+                <Button
+                  onClick={() => deleteDepth(i)}
+                  variant="outlined"
+                  startIcon={<Delete />}
+                >
+                  Delete
+                </Button>
               </StyledTableRow>
             ))}
           </TableBody>
